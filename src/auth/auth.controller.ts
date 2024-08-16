ac9f0a500c0d2b531a6b92f09ad1f2a681b55ba3
Maksim Shuklin <88307988+MakShuk@users.noreply.github.com>
2024-08-16 18:56:49 +0300

    chore: Add class-transformer npm dependency

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,29 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterDto } from './auth.dto';
+import { Public } from 'src/decorators/custom.decorator';
 
-
+@UsePipes(new ValidationPipe({ whitelist: true }))
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Public()
   @Post('register')
   async register(@Body() dto: RegisterDto) {
     return this.authService.register(dto);
   }
 
+  @Public()
   @Post('login')
   async login(@Body() { email, password }: LoginDto) {
-    console.log(`email: ${email}, password: ${password}`);
-     const { id } = await this.authService.validatePassword(password, email);
+    const { id } = await this.authService.validatePassword(password, email);
     return this.authService.login(String(id));
   }
 }
--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,10 +1,44 @@
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  MaxLength,
+  Matches,
+} from 'class-validator';
+
 export class RegisterDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @MaxLength(20, { message: 'Password must be at most 20 characters long' })
+  @Matches(/(?=.*[A-Z])/, {
+    message: 'Password must contain at least one uppercase letter',
+  })
+  @Matches(/(?=.*[a-z])/, {
+    message: 'Password must contain at least one lowercase letter',
+  })
+  @Matches(/(?=.*[0-9])/, {
+    message: 'Password must contain at least one number',
+  })
+  @Matches(/(?=.*[!@#$%^&*])/, {
+    message: 'Password must contain at least one special character',
+  })
   password: string;
+
+  @IsString()
+  @MinLength(3, { message: 'Username must be at least 3 characters long' })
+  @MaxLength(20, { message: 'Username must be at most 20 characters long' })
   username: string;
 }
 
 export class LoginDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @MaxLength(20, { message: 'Password must be at most 20 characters long' })
   password: string;
 }
